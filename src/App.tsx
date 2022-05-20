7c504d09ea8645b42d12edf4e649ad4583853028
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-20 17:22:34 -0300

    Created summary and TransactionsTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Header } from './components/Header/Index'
+import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   )
