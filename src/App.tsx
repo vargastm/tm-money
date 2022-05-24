107bea7e1ed3d250db4ae4f7e78d943278b96c1b
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-24 14:47:03 -0300

    Comment import from header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import { Dashboard } from './components/Dashboard'
 import Modal from 'react-modal'
-{/* import { Header } from './components/Header'*/}
-import { GlobalStyle } from './styles/global'
+import { Dashboard } from './components/Dashboard'
+//import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
 import { TransactionsProvider } from './hooks/useTransactions'
+
+import { GlobalStyle } from './styles/global'
+
 
 Modal.setAppElement('#root')
 
