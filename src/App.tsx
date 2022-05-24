b3c6bd987e91c57af29f2bce41e31d4d2b4019b9
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-24 14:57:53 -0300

    Removed comment to header import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard'
-//import { Header } from './components/Header'
+import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
 import { TransactionsProvider } from './hooks/useTransactions'
 
@@ -23,7 +23,7 @@ export function App() {
   
   return (
     <TransactionsProvider>
-{/*      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />*/}
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
 
